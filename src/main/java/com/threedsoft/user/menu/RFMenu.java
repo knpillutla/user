c03c741326a3fd282a9ec7efd3c68caeb2440cb6
Krishna Pillutla <knpillutla@gmail.com>
2018-12-22 09:05:59 -0500

    Adding

--- src/main/java/com/threedsoft/user/menu/RFMenu.java
+++ src/main/java/com/threedsoft/user/menu/RFMenu.java
@@ -29,20 +29,21 @@ public class RFMenu {
 				//new RFScreenResource("pickToTote", "Pick To Tote", "RW",0,30,15,null,null);
 		String nextPickUrl = "https://the3dsoft.com/picking/v1/" + busName + "/" + locnNbr + "/picks/next/{userId}";
 		String updateRecordUrl = "https://the3dsoft.com/picking/v1/" + busName + "/" + locnNbr + "/picks" + "/{id}";
+		String endToteUrl = "https://the3dsoft.com/picking/v1/" + busName + "/" + locnNbr + "/picks" + "/{id}/endTote";
 		List<RFFieldResource> fieldList = new ArrayList();
 		fieldList.add(RFFieldResource.createHiddenField("busName", busName));
 		fieldList.add(RFFieldResource.createHiddenField("locnNbr", locnNbr));
-		fieldList.add(RFFieldResource.createDataTriggerHiddenField("id", "Pick Id","int", "10",nextPickUrl,"No Picks Found, try again..."));
+		fieldList.add(RFFieldResource.createDataTriggerLabelField("id", "Pick Id","int", "10",nextPickUrl,"No Picks Found, try again..."));
 		fieldList.add(RFFieldResource.createTextField("toteNbr", "Enter Tote","string", "20"));
 		fieldList.add(RFFieldResource.createLabelField("locnBrcd", "Location Brcd","string", "7"));
 		fieldList.add(RFFieldResource.createTextFieldWithValidation("scanLocnBrcd", "Enter Location Barcode:","int", "7","locnBrcd","invalid location barcode"));
 		fieldList.add(RFFieldResource.createLabelField("upc", "UPC","string", "20"));
 		fieldList.add(RFFieldResource.createTextFieldWithValidation("scanUpc", "Enter UPC:","string", "20","upc","invalid upc"));
 		fieldList.add(RFFieldResource.createLabelField("qty", "Qty:","int", "3"));
-		fieldList.add(RFFieldResource.createActionTextFieldWithValidation("scanQty", "Enter Qty:","string", "3","Y","qty","",updateRecordUrl,"id:id,toteNbr:toteNbr,locnBrcd:locnBrcd,upc:upc,scanQty:qty"));
+		fieldList.add(RFFieldResource.createActionTextFieldWithValidation("scanQty", "Enter Qty:","string", "3","qty","invalid qty",updateRecordUrl,"id:id,toteNbr:toteNbr,locnBrcd:locnBrcd,upc:upc,scanQty:qty","Y"));
 
 		rfScreen.setRfFieldResourceList(fieldList);
-		RFButtonResource buttonResource = new RFButtonResource("endTote","End Tote","Are you sure you want to end tote?","", "","toteNumber","N");
+		RFButtonResource buttonResource = new RFButtonResource("endTote","End Tote","Are you sure you want to end tote?",endToteUrl, "id:id,toteNbr:toteNbr","toteNumber","N");
 		rfScreen.setButtonResource(buttonResource);
 		return rfScreen;
 	}
@@ -60,7 +61,7 @@ public class RFMenu {
 		fieldList.add(RFFieldResource.createLabelField("qty", "Qty:","int", "3"));
 		fieldList.add(RFFieldResource.createActionTextFieldWithValidation("scanQty", "Enter Qty:","string", "3","Y","qty","",updateRecordUrl,"id:id,toteNbr:toteNbr,locnBrcd:locnBrcd,upc:upc,scanQty:qty"));
 		fieldList.add(RFFieldResource.createLabelField("packageNbr", "Package Number:","string", "20"));
-		fieldList.add(RFFieldResource.createActionTextFieldWithValidation("scanPackageNbr", "Enter Package Nbr:","string", "20","Y","packageNbr","",updateRecordUrl,"id:id,toteNbr:toteNbr,upc:upc,scanQty:qty,packageNbr:packageNbr"));
+		fieldList.add(RFFieldResource.createActionTextFieldWithValidation("scanPackageNbr", "Enter Package Nbr:","string", "20","packageNbr","invalid package",updateRecordUrl,"id:id,toteNbr:toteNbr,upc:upc,scanQty:qty,packageNbr:packageNbr","Y"));
 
 		rfScreen.setRfFieldResourceList(fieldList);
 		RFButtonResource buttonResource = new RFButtonResource("exit","Exit","Are you sure you want to Exit?","", "","","Y");
@@ -79,7 +80,7 @@ public class RFMenu {
 		fieldList.add(RFFieldResource.createLabelField("estimatedWt", "EstimatedWt:","numeric", "3,2"));
 		fieldList.add(RFFieldResource.createTextField("actualWt", "ActualWt:","numeric", "3,2"));
 		rfScreen.setRfFieldResourceList(fieldList);
-		RFButtonResource buttonResource = new RFButtonResource("ship","Ship Package","",shipPackageUrl, "packageNbr,estimatedWt,actualWt","packageNbr","N");
+		RFButtonResource buttonResource = new RFButtonResource("ship","Ship Package","",shipPackageUrl, "packageNbr:packageNbr,estimatedWt:estimatedWt,actualWt:actualWt","packageNbr","N");
 		rfScreen.setButtonResource(buttonResource);
 		return rfScreen;
 	}
